2f0396c7a98a50f5321dac1a0a579720a358493c
Leah Benitez <benteriordesign@gmail.com>
2021-06-11 09:12:17 -0400

    Created form and empty handler methods

--- city-search/src/Components/Home.js
+++ city-search/src/Components/Home.js
@@ -10,12 +10,35 @@ class Home extends Component {
         }
     }
 
+    handleZipcodeChange(event) {
+
+    }
+    
+    formSubmitHandler(event) {
+
+    }
+
     render() {
         return(
             <div id="page-wrapper">
                 <div id="title-div">
                     <h1>Welcome to the Zipcode Search App</h1>
                 </div>
+
+                <section id="section-wrapper">
+                    
+                    <form onSubmit={this.formSubmitHandler}>
+                        <label>Enter a Zipcode: </label>
+                        <input placeholder="Try New York"
+                            value={this.state.zipcode}
+                            onChange={this.handleZipcodeChange}
+                            name="zipcode"
+                            maxLength="5"
+                            minLength="5">
+                        </input>
+                        <button type="submit">Submit</button>
+                    </form>
+                </section>
             </div>
         );
     }
